38e0d5f3d4ec43e3763348fe54129d3e26c71f3b
Samihan Jadhav <samihanjadhav@gmail.com>
2026-02-11 15:18:53 +0000

    Add FRONTEND_URL env variable support

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const app = require('./app');
 
 const PORT = process.env.PORT || 5000;
@@ -6,7 +7,7 @@ const cors = require("cors");
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://fsd-115-samihan-codes.vercel.app/"
+    process.env.FRONTEND_URL
   ],
   credentials: true
 }));
